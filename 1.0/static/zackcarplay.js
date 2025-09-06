720ea14dc40698413371f227c8f3f4199366daa8
userpetitpain <rosemickey150@gmail.com>
2025-09-06 23:01:24 +0200

    real initial commit

--- 1.0/static/zackcarplay.js
+++
@@ -1,9 +0,0 @@
-clignotants  = () => {
-  fetch('/test_shortcut')
-  console.log('Clignotants action triggered');
-}
-
-reload_shortcut = () => {
-  fetch('/reload_shortcut')
-  console.log('Reload action triggered');
-}
